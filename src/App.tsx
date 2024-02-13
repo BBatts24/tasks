54681749fd6a52bc687f0ef7403b3074d5834702
BBatts24 <philipcolborn@gmail.com>
2024-02-13 16:50:05 -0500

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Philip Colborn UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript Philip Colborn
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
