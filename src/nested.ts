3b8876e67ce29cbca163c388bd8668073189414c
BBatts24 <philipcolborn@gmail.com>
2024-02-26 00:37:49 -0500

    first nest

--- src/nested.ts
+++ src/nested.ts
@@ -6,7 +6,9 @@ import { Question, QuestionType } from "./interfaces/question";
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-    return [];
+    return questions.filter(
+        (ques: Question): boolean => ques.published === true
+    );
 }
 
 /**
@@ -15,7 +17,12 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    return questions.filter(
+        (ques: Question): boolean =>
+            ques.body !== "" &&
+            ques.expected !== "" &&
+            ques.options.length !== 0
+    );
 }
 
 /***
