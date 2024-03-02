fd1a25b6e6698d3116aed81fbfa472c877c0dd97
BBatts24 <philipcolborn@gmail.com>
2024-03-02 14:17:04 -0500

    take 3

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,12 +1,10 @@
+/*
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-
-/**
+//import { Button } from "react-bootstrap";
  * Here is a helper function you *must* use to "roll" your die.
  * The function uses the builtin `random` function of the `Math`
  * module (which returns a random decimal between 0 up until 1) in order
  * to produce a random integer between 1 and 6 (inclusive).
- */
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
@@ -14,3 +12,4 @@ export function d6(): number {
 export function TwoDice(): JSX.Element {
     return <div>Two Dice</div>;
 }
+*/
